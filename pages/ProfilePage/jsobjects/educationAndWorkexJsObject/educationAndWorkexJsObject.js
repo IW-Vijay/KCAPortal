ae95c2abcc81bcce372daab1e8965fb911b61e87
Vijay Verma <vijay.verma@innowhyte.com>
2024-11-01 04:04:41 +0000

    testing

--- pages/ProfilePage/jsobjects/educationAndWorkexJsObject/educationAndWorkexJsObject.js
+++ pages/ProfilePage/jsobjects/educationAndWorkexJsObject/educationAndWorkexJsObject.js
@@ -1,10 +1,11 @@
 export default {
 	async educations() {
+		let educationWithNames = null;
 		await fetchEducation.run();
 		let educationList = fetchEducation.data;
 
 		// Loop through the workex list and replace organization_id with the fetched name
-		let educationWithNames = await Promise.all(educationList.map(async (education) => {
+		educationWithNames = await Promise.all(educationList.map(async (education) => {
 			await fetchInstitutionName.run({ "institution_id": education.institution_id });
 			showAlert(education.organization_id);
 
@@ -20,11 +21,12 @@ export default {
 		return { "education": educationWithNames };
 	},
 	async workexs() {
+		let workexWithNames = null;
 		await fetchWorkex.run();
 		let workexList = fetchWorkex.data;
 
 		// Loop through the workex list and replace organization_id with the fetched name
-		let workexWithNames = await Promise.all(workexList.map(async (workex) => {
+		workexWithNames = await Promise.all(workexList.map(async (workex) => {
 			await fetchOrganizationName.run({ "organization_id": workex.organization_id });
 			showAlert(workex.organization_id);
 
